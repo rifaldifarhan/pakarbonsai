c1e5be4a5e80ddf4e977afa8590b1c9e3be44f7e
Moh. Farhan Rifaldi S <rifaldifarhan24@gmail.com>
2023-06-12 19:15:08 +0700

    perbaiki info pakar

--- application/modules/User/views/info_pakar.php
+++ application/modules/User/views/info_pakar.php
@@ -1,4 +1,4 @@
-<header class="bg-image" style="background-image: url('<?php echo base_url(); ?>desain/images/bg-3.jpg'); height: 100vh;">
+<header class="bg-image" style="background-image: url('<?php echo base_url(); ?>desain/images/bg-3.jpg');">
 
     <div class="container mt-5">
 
@@ -8,32 +8,38 @@
 
                 <div class="card p-3 py-4">
 
+                    <div class="text-center mt-3">
+                        <h3 class="mb-4">PAKAR</h3>
+                    </div>
+
                     <div class="text-center">
-                        <img src="<?php echo base_url('desain/images/logo bonsai.png'); ?>" width="200" class="rounded-circle">
+                        <img src="<?php echo base_url('desain/images/ibu irma.jpeg'); ?>" width="200" style="border-radius: 10px;">
                     </div>
 
                     <div class="text-center mt-3">
                         <!-- <span class="bg-secondary p-1 px-4 rounded text-white">Pro</span> -->
-                        <h5 class="mt-3 mb-2">Alexender Schidmt</h5>
-                        <span>UI/UX Designer</span>
+                        <h5 class="mt-3">Irma Wardati, SP, MP</h5>
+                        <h6 class="mt-3 mb-4">NIP : 19691219 200003 2 001</h6>
+                        <span>Dosen Jurusan Produksi Pertanian Prodi Budidaya Tanaman Perkebunan</span>
+                        <span>Politeknik Negeri Jember</span>
 
                         <div class="px-4 mt-1 mb-3 mt-3">
-                            <p class="fonts">Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                            <!-- <p class="fonts">Dosen Jurusan Produksi Pertanian Prodi Budidaya Tanaman Perkebunan</p> -->
 
                         </div>
 
-                        <ul class="social-list">
+                        <!-- <ul class="social-list">
                             <li><i class="fa-brands fa-facebook"></i></li>
                             <li><i class="fa-brands fa-instagram"></i></li>
                             <li><i class="fa-brands fa-whatsapp"></i></li>
                             <li><i class="fa-brands fa-linkedin"></i></li>
-                        </ul>
+                        </ul> -->
 
-                        <div class="buttons">
+                        <!-- <div class="buttons">
 
                             <button class="btn btn-outline-primary px-4">Pesan</button>
                             <button class="btn btn-primary px-4 ms-3">Kontak</button>
-                        </div>
+                        </div> -->
 
 
                     </div>
